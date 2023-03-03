18f0b5c955d3aecd3933e6891f4207a2c9e1d365
bryaanls <bryanluccas@hotmail.com>
2023-03-03 15:57:00 -0300

    menu mobile implementation

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+function menuMobile() {
+    const btnMenu = document.getElementById('close-menu');
+    const menuTxt = document.getElementById('menu-mobile');
+    const menu = document.querySelector('.menu')
+    let click = 0;
+    btnMenu.addEventListener('click', () => {
+
+        if (click % 2 === 0) {
+            menu.style.display = 'block';
+            menu.classList.add('entradaMenu');
+            menuTxt.classList.add('entradaTxt');
+            menu.classList.remove('saidaMenu');
+            menuTxt.classList.remove('saidaTxt');
+            btnMenu.innerText = '☓'
+
+
+        }
+        if (click % 2 === 1) {
+            menu.classList.remove('entradaMenu');
+            menu.classList.add('saidaMenu');
+            menuTxt.classList.add('saidaTxt');
+            menuTxt.classList.remove('entradaTxt');
+            btnMenu.innerText = '☰'
+
+        }
+        click++;
+    })
+}
+
+menuMobile();
